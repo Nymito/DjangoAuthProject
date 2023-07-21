c2b2f5228082d7e5295cddeda12cb5b1a7028cf2
Nymito <gianni.jouve@gmail.com>
2023-07-22 00:19:02 +0200

    created django project linked to react app

--- AuthApp/urls.py
+++ AuthApp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('AuthApp.urls')),
+    path('', views.home, name="home"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
 ]
---
+++ AuthApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+def signup(request):
+    return render(request, "AuthApp/signup.html")
+
+def signin(request):
+    return render(request, "AuthApp/signin.html")
+
+def signout(request):
+    return render(request, "AuthApp/signout.html")
